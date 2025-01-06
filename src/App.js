a04e3747b1116356fa8af6547d979584a4eb6e07
Bruno Desktop <carecsnay1@gmail.com>
2025-01-05 21:02:34 -0300

    Feat: Add cors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import TaskItem from "./components/TaskItem";
 
 function App() {
-    const [message, setMessage] = useState("Initial value");
+    const [tasks, setTasks] = useState([{}]);
 
-    const handlerMessage = () => {
-        setMessage("Update Value");
+    //Pegando tarefas do BD
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("https://task-manager-fsc-backend.onrender.com/tasks"); //pegando tarefas da nossa API
+            setTasks(data); //setando tarefas no estado!
+        } catch (error) {
+            console.log(error);
+        }
     };
+
+    useEffect(() => {
+        fetchTasks(); //useEffect não pode retornar uma promisse (async)
+    }, []);
 
     return (
         <>
-            <h1>{message}</h1>
-            <button onClick={handlerMessage}>My button</button>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
         </>
     );
 }
