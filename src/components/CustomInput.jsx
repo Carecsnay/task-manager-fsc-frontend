efc5a33ef779ee95971e6e344448bf2ab818d177
Bruno Nitro 5 <carecsnay1@gmail.com>
2025-01-09 15:24:30 -0300

    Fix: clean input before add in BD

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -5,8 +5,8 @@ const CustomInput = ({ label, value, onChange }) => {
         <>
             {/* label pode ser dinâmica (pode ser email ou senha por exemplo) */}
             <div className="custom-input-container">
-                <input type="text" className="custom-input" onChange={(e) => onChange(e)} /> {/*Pegando o valor do estado do on change do addtask e guardando*/}
-                {/* se tiver uma label e existir alguma coisa mela vai utilizar a classe shrink se não tiver uma label ela não vai mostrar nada "null" a label sempre tem a classe custom-input-label = caso tenha label + alguma coisa -> shrink custom-input-label*/}
+                <input type="text" value={value} className="custom-input" onChange={(e) => onChange(e)} />{" "}
+                {/*Pegando o valor do estado do on change do addtask e guardando, o value serve para atualizar o input quando tiver uma nova tarefa (limpa input) se tiver uma label e existir alguma coisa mela vai utilizar a classe shrink se não tiver uma label ela não vai mostrar nada "null" a label sempre tem a classe custom-input-label = caso tenha label + alguma coisa -> shrink custom-input-label*/}
                 {label ? (
                     <label className={`${value.length > 0 ? "shrink" : ""} custom-input-label`}>{label}</label>
                 ) : null}
