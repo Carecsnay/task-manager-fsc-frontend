9b2ec1a3c04fa63b046258cb721110c68d42286f
Bruno Nitro 5 <carecsnay1@gmail.com>
2025-01-09 15:24:04 -0300

    Feat: Find new tasks added

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -29,7 +29,7 @@ function Tasks() {
             <h2>Minhas tarefas</h2>
             <div className="last-tasks">
                 <h3>Últimas tarefas</h3>
-                <AddTask />
+                <AddTask fetchTasks={fetchTasks} />
                 <div className="tasks-list">
                     {tasks
                         .filter((task) => task.isCompleted === false)
