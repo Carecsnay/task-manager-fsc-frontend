346ee36a6819bdaf59255ccdbf63493391ef770a
Bruno Desktop <carecsnay1@gmail.com>
2025-01-10 20:11:48 -0300

    Feat: SideBar component created!

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,19 @@
+import CustomButton from "./CustomButton";
+
+import "./SideBar.scss";
+import logo from "../assets/images/logo.png";
+
+const SideBar = () => {
+    return (
+        <div className="sidebar-GoContainer">
+            <div className="logo">
+                <img src={logo} alt="logo da full stack club" />
+            </div>
+            <div className="sing-out">
+                <CustomButton>Sair</CustomButton>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
