979416a055ba1ab7b7b9770d36a1ac1848abb656
Bruno Nitro 5 <carecsnay1@gmail.com>
2025-01-13 15:51:32 -0300

    perf: add fetchTasks to useCallback

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,7 @@
 //Deixar um arquivo com .JSX ajuda no autocomplete, portanto, sempre que usar JS lembre-se de utilizar a extensão correta.
 
 import axios from "axios";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 
 import "./Tasks.scss";
 import TaskItem from "./TaskItem";
@@ -11,25 +11,27 @@ import { toast } from "react-toastify";
 function Tasks() {
     const [tasks, setTasks] = useState([{}]);
 
-    //Pegando tarefas do BD
-    const fetchTasks = async () => {
+    //Pegando tarefas do BD. O UseCallback é semelhante ao useMemo, a diferença é que ele guarda uma função em memória e só atualiza quando o array de dependência é alterado. 
+    const fetchTasks = useCallback(async () => {
         try {
             const { data } = await axios.get("https://task-manager-fsc-backend.onrender.com/tasks"); //pegando tarefas da nossa API
             setTasks(data); //setando tarefas no estado!
         } catch (_error) {
             toast.error("Não foi possível recuperar as tarefas no momento, tente novamente mais tarde!");
         }
-    };
+    }, []);
 
     useEffect(() => {
         fetchTasks(); //useEffect não pode retornar uma promise (async)
-    }, []);
+    }, [fetchTasks]);
+
     const lastTask = useMemo(() => {
         //o useMemo é utilizado quando o useState das tasks muda, evitando calculo desnecessário dos map do tasks-list
         return tasks.filter((task) => task.isCompleted === false);
     }, [tasks]);
 
     const completedTasks = useMemo(() => {
+        //guarda um resultado de uma função em memória
         return tasks.filter((task) => task.isCompleted); //mesmo que colocar task.isCompleted === true
     }, [tasks]);
 
