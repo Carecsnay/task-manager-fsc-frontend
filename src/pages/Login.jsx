ad09afe3c0087c28629937a48889be4d04be4566
Bruno Desktop <carecsnay1@gmail.com>
2025-01-12 22:50:58 -0300

    Feat: Add function to sign in on application

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 import "./Login.scss";
 import logo from "../assets/images/logo-2.png";
 import CustomButton from "../components/CustomButton";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSingInClick = () => {
+        navigate("/");
+    };
+
     return (
         <div className="login-container">
             <img src={logo} alt="Logo da Full Stack Club" />
-            <CustomButton>Entrar</CustomButton>
+            <div className="button-container">
+                <CustomButton onClick={handleSingInClick}>Entrar</CustomButton>
+            </div>
         </div>
     );
 };
