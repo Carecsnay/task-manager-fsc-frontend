92007697232f3fadfd3deb20d74a715b3dcbe15e
Bruno Desktop <carecsnay1@gmail.com>
2025-01-12 23:00:25 -0300

    Feat: Add router to SideBar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,22 @@
 import CustomButton from "./CustomButton";
+import { useNavigate } from "react-router-dom";
 
 import "./SideBar.scss";
 import logo from "../assets/images/logo.png";
 
 const SideBar = () => {
+    const navigate = useNavigate();
+
+    const handleSingOutClick = () => {
+        navigate("/login");
+    };
     return (
         <div className="sidebar-container">
             <div className="logo">
                 <img src={logo} alt="logo da full stack club" />
             </div>
             <div className="sing-out">
-                <CustomButton>Sair</CustomButton>
+                <CustomButton onClick={handleSingOutClick}>Sair</CustomButton>
             </div>
         </div>
     );
